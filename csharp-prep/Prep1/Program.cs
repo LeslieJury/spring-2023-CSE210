2f9013c7b97a4a64ef8229433ee4327445d33f9e
Leslie Jury <lesliejury3@gmail.com>
2023-04-22 18:53:01 -0600

    James Bond Update!

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        
+        // Type your first and last name
+        Console.WriteLine("What is your name?: ");
+
+        // Assign the input to a variable
+        string user_name = Console.ReadLine();
+
+        // Find the index of the space so we can seperate the first and last name
+        int space_index = (user_name.IndexOf(" "));
+
+        // Assign the last word in the user name as the variable last_name
+        string last_name = user_name.Substring(space_index);
+
+        // Find first name (Not essential to this assignment but helpful for learning about Substrings)
+        int length_full_name = user_name.Length - last_name.Length;
+        string first_name = user_name.Substring(0, length_full_name);
+
+        // Print everything out
+        Console.WriteLine("Your name is " + last_name + ", " + first_name + last_name);
     }
 }
