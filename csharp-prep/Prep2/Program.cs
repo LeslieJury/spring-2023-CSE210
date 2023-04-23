2b0a646df3e50da8bd4363dd380d1b8976be077f
Leslie Jury <lesliejury3@gmail.com>
2023-04-22 19:14:06 -0600

    New grade system

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade?: ");
+
+        string your_grade = Console.ReadLine();
+
+        int num_grade;
+
+        num_grade = (Convert.ToInt32(your_grade));
+
+        if (num_grade > 90)
+        {
+            Console.WriteLine("You have an A!");
+        }
+        else if (num_grade > 80)
+        {
+            Console.WriteLine("You have a B");
+        }
+        else if (num_grade > 70)
+        {
+            Console.WriteLine("You have a C");
+        }
+        else if (num_grade > 60)
+        {
+            Console.WriteLine("You have a D");
+        }
+        else
+        {
+            Console.WriteLine("You have an F");
+
+        }
     }
 }
