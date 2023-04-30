13fc1bb90b9a538774809471542f3eb3dd61047f
Leslie Jury <lesliejury3@gmail.com>
2023-04-29 20:48:45 -0600

    Abstraction Example and number 4 update

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
+
 
 class Program
 {
     static void Main(string[] args)
     {
+        List<int> nums = new List<int>();
+
+        Console.WriteLine("Please enter some numbers for our list, select 0 when you are finished!: ");
+        string userInput = Console.ReadLine();
+        int numInput = (Convert.ToInt32(userInput)); 
+
+        while (numInput != 0)
+        {
+            nums.Add(numInput);
+            
+        }
+
         Console.WriteLine("Hello Prep4 World!");
+        for (int i = 0; i < nums.Count; i++)
+        {
+            Console.WriteLine(nums[i]);
+        }
     }
 }
