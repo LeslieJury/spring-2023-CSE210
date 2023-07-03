e3ac3f11d8962e8df6cf569d32dbf3d25e1fd813
Leslie Jury <lesliejury3@gmail.com>
2023-07-02 19:35:18 -0600

    Added a menu class

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,64 +7,19 @@ public class Program
 {
     private static Journal journal;
     private static Prompt prompt;
-    private static FileAction fileAction;
+
 
 
     public static void Main(string[] args)
     {
         prompt = new Prompt();
         journal = new Journal(prompt);
-        fileAction = new FileAction("default.json");
-
-    // Load the default.json file if it exists
-
-        fileAction.Load(journal, prompt);
 
         bool exit = false;
-        while (!exit)
+        while (exit ==  false)
         {
-            Console.WriteLine("MENU\n1. Create new journal entry\n2. Write a prompt\n3. Remove a prompt\n4. Display journal entries\n5. Load a Journal\n6. Save a Journal\n7. Quit\n\nEnter your choice: ");
+            
 
-
-            if (int.TryParse(Console.ReadLine(), out int choice))
-            {
-                Console.WriteLine();
-
-                switch (choice)
-                {
-                    case 1:
-                        journal.Write();
-                        break;
-                    case 2:
-                        prompt.Add();
-                        break;
-                    case 3:
-                        prompt.Remove();
-                        break;
-                    case 4:
-                        journal.Display();
-                        break;
-                    case 5:
-                        fileAction.Load(journal, prompt);
-                        break;
-                    case 6:
-                        fileAction.Save(journal, prompt);
-                        break;
-                    case 7:
-                        exit = true;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice. Please try again.");
-                        break;
-                }
-
-                Console.WriteLine();
-            }
-            else
-            {
-                Console.WriteLine("Invalid choice. Please try again.");
-                Console.WriteLine();
-            }
         }
     }
 }
--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -14,36 +14,33 @@ public class Prompt
     private void InitializePrompts()
     {
         prompts.Add("What is something you did that made you feel happy today?");
+
         prompts.Add("What is one interesting thing you've experienced or seen today?");
+
         prompts.Add("Did you see any friends or family today?");
+        
         prompts.Add("Name your favorite childhood toy and why.");
     }
 
 
-    public string Random()
-    {
-        Random random = new Random();
-        int index = random.Next(prompts.Count);
-        return prompts[index];
-    }
-
     public void Add()
     {
-        Console.Write("Add a new prompt?: ");
+        Console.Write("What's your new prompt?: ");
         string newPrompt = Console.ReadLine();
         prompts.Add(newPrompt);
-        Console.WriteLine("Prompt added successfully.");
+        Console.WriteLine("Your prompt has been added!");
     }
-
     public void Remove()
     {
-        Console.WriteLine("Entry List");
+        Console.WriteLine("List of prompts so far: ");
         for (int i = 0; i < prompts.Count; i++)
         {
             Console.WriteLine($"{i + 1}. {prompts[i]}");
         }
 
-        Console.Write("Enter the number of the prompt to remove: ");
+        Console.Write("Which prompt would you like to remove?: ");
+
+        // Remember tutor's notes about try parse. Better than parse for this because it returns a boolean of it it succeeds.
         if (int.TryParse(Console.ReadLine(), out int selectedPrompt))
         {
             if (selectedPrompt >= 1 && selectedPrompt <= prompts.Count)
@@ -54,12 +51,19 @@ public class Prompt
             }
             else
             {
-                Console.WriteLine("Invalid prompt number.");
+                Console.WriteLine("That propmt seems to not exist!");
             }
         }
         else
         {
-            Console.WriteLine("Invalid input.");
+            Console.WriteLine("Invalid input!");
         }
     }
+            public string Random()
+    {
+        Random random = new Random();
+        int index = random.Next(prompts.Count);
+        return prompts[index];
+    }
+
 }
